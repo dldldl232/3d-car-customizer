3adfc23cca3a6516490e38356d4c4e57c0a74940
Seoyeon Kim <seoyeonkim0404@gmail.com>
2025-07-16 19:03:22 +0900

    Add user authentication enhancements: implement current user retrieval with JWT validation and session management in auth.py. Update dependencies in pyproject.toml and poetry.lock for password hashing and settings management.

--- app/auth.py
+++ app/auth.py
@@ -3,6 +3,9 @@ from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from app.config import get_settings
 from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+from sqlmodel import Session
+from app.db import get_user, get_session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -45,3 +48,23 @@ def decode_access_token(token: str):
         return payload
     except JWTError:
         return None
+
+# for user updating their info
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    session: Session = Depends(get_session)
+):
+    payload = decode_access_token(token)
+    if not payload or "sub" not in payload:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED
+            detail="Invalid authentication credentials",
+        )
+    username = payload["sub"] # question what sub is
+    user = get_user(username, session)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND
+            detail="User not found",
+        )
+    return user
